a0ba190530b1b4eb94a5c8517e76f1e95a97fbf6
DESKTOP-JNM0P6G\Matheus <avelinosantos09@gmail.com>
2023-01-20 11:14:47 -0300

    Início e preparação para o serviço de estoque do salão - Preparação da Interface, Service e Controller; - Métodos da Service ainda não possuem verificação; - Ainda não é possível testar os métodos pela controler;
    - *Próximos passos*:
    - Implementar Controller para testar métodos;
    - Implementar verificação para a adição de itens no estoque;
    - Implementar o restante dos métodos do estoque
    - Implementar testes

---
+++ Hair.Application/Interfaces/IStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Hair.Application.Common;
+using Hair.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hair.Application.Interfaces
+{
+    public interface IStorageService
+    {
+        public BaseDto ManageStorage(int itemCount, Guid itemId, double itemPrice, bool addItem, bool removeItem, bool confirmed);
+    }
+}
---
+++ HairSystem/Controllers/StorageController.cs
@@ -0,0 +1,24 @@
+﻿using Hair.Application.Services;
+using Hair.Domain.Entities;
+using Hair.Repository.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StorageController : ControllerBase
+    {
+        private readonly StorageService _storageService;
+        private readonly StorageRepository _storageRepository;
+
+        public StorageController(StorageRepository storageRepository)
+        {
+            _storageRepository = storageRepository;
+            _storageService = new StorageService(_storageRepository);
+        }
+        [HttpGet]
+        [Route("StorageManagement")]
+
+    }
+}
