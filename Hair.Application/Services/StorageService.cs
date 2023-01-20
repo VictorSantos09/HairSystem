9bd5592fcd61ab78ab95f9bfa9cb082f0c047e94
DESKTOP-JNM0P6G\Matheus <avelinosantos09@gmail.com>
2023-01-20 11:14:47 -0300

    Início e preparação para o serviço de estoque do salão - Preparação da Interface, Service e Controller; - Métodos da Service ainda não possuem verificação; - Ainda não é possível testar os métodos pela controler;
    - *Próximos passos*:
    - Implementar Controller para testar métodos;
    - Implementar verificação para a adição de itens no estoque;
    - Implementar o restante dos métodos do estoque
    - Implementar testes

---
+++ Hair.Application/Services/StorageService.cs
@@ -0,0 +1,39 @@
+﻿using Hair.Application.Common;
+using Hair.Domain.Entities;
+using Hair.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hair.Application.Services
+{
+
+    public class StorageService
+    {
+        private readonly IBaseRepository<SaloonItemEntity> _saloonItem;
+        //List<string> storage = new List<string>();   
+        public StorageService(IBaseRepository<SaloonItemEntity> saloonItem)
+        {
+            _saloonItem = saloonItem;
+        }
+        public BaseDto AddItem(string name, double price, int quantityAvaliable, Guid itemId)
+        {
+            var item = new SaloonItemEntity(name, price, quantityAvaliable);
+            _saloonItem.Add(item);
+            return new BaseDto(200, "Item adicionado com sucesso");
+        }
+        public BaseDto GetItems()
+        {
+            _saloonItem.GetAll();
+            return new BaseDto(200, "Exibindo todos os itens");
+        }
+        public BaseDto GetItemsId(Guid itemId)
+        {
+            _saloonItem.GetById(itemId);
+            return new BaseDto(200, "Item encontrado");
+        }
+    }
+}
