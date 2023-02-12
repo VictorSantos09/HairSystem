275b19b1ba676df7a27e60bf71e8438227bfc93e
Victor Santos <victorluizsantos49@gmail.com>
2023-02-11 21:02:01 -0300

    melhora userRepo

--- Hair.Repository/Repositories/UserRepository.cs
+++ Hair.Repository/Repositories/UserRepository.cs
@@ -23,8 +23,22 @@ namespace Hair.Repository.Repositories
         {
             using (var conn = new SqlConnection(DataAccess.DBConnection))
             {
-                var query = new SqlCommand($"INSERT INTO {TableName} (ID, SALOON_NAME, OWNER_NAME, PHONE_NUMBER, EMAIL, PASSWORD, CNPJ, HAIRCUT_TIME, HAIRCUT_PRICE) VALUES ('{user.Id}', '{user.SaloonName}','{user.OwnerName}','{user.PhoneNumber}','{user.Email}','{user.Password}', '{user.CNPJ}', 02/02/2004 ,'{user.Prices.Hair}')", conn);
+                var query = new SqlCommand($"INSERT INTO {TableName} VALUES (@ID, @SALOON_NAME, @OWNER_NAME, @PHONE_NUMBER, @EMAIL," +
+                    $" @PASSWORD, @CNPJ, @HAIRCUT_TIME, @HAIRCUT_PRICE)", conn);
+                
                 conn.Open();
+
+                query.Parameters.AddWithValue("@ID", user.Id);
+                query.Parameters.AddWithValue("@SALOON_NAME", user.SaloonName);
+                query.Parameters.AddWithValue("@OWNER_NAME", user.OwnerName);
+                query.Parameters.AddWithValue("@PHONE_NUMBER", user.PhoneNumber);
+                query.Parameters.AddWithValue("@EMAIL", user.Email);
+                query.Parameters.AddWithValue("@PASSWORD", user.Password);
+                query.Parameters.AddWithValue("@CNPJ", user.CNPJ);
+                query.Parameters.AddWithValue("@HAIRCUT_TIME", "02/04/2004"); // colocar como datetime ou string tanto no C# quanto SQL
+                query.Parameters.AddWithValue("@HAIRCUT_PRICE", user.Prices.Hair); // Resolver questao de ser necessario dizer a propriedade. Ex: Prices.Hair, deve ser apenas user.Prices
+
+                query.ExecuteNonQueryAsync();
             }
         }
 
