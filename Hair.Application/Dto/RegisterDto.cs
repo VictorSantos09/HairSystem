f81277bd86bb694181208a9f7b48e45fe4718b7b
Victor Santos <victorluizsantos49@gmail.com>
2023-02-12 15:51:12 -0300

    correção registerDto e pagina cadastro quase pronta

--- Hair.Application/Dto/RegisterDto.cs
+++ Hair.Application/Dto/RegisterDto.cs
@@ -4,13 +4,12 @@ namespace Hair.Application.Dto
 {
     public class RegisterDto
     {
-        public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public AddressEntity Address { get; set; }
         public HaircutePriceEntity HaircutePrice { get; set; }
         public string CNPJ { get; set; }
-        public string OwnerName { get; set; }
+        public string Name { get; set; }
         public string Password { get; set; }
         public string SaloonName { get; set; }
 
