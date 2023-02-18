5c0679bcb565359176784cf00b5745884ae69327
DESKTOP-VRG87DV\Matheus <avelinosantos09@gmail.com>
2023-02-18 20:54:06 -0300

    Rebase

--- Hair.Application/Services/StorageService.cs
+++ Hair.Application/Services/StorageService.cs
@@ -1,6 +1,7 @@
 ﻿using Hair.Application.Common;
 using Hair.Domain.Entities;
 using Hair.Repository.Interfaces;
+using Hair.Repository.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,27 +14,33 @@ namespace Hair.Application.Services
 
     public class StorageService
     {
-        private readonly IBaseRepository<SaloonItemEntity> _saloonItem;
+        private readonly StorageRepository _saloonItem;
         //List<string> storage = new List<string>();   
-        public StorageService(IBaseRepository<SaloonItemEntity> saloonItem)
+        public StorageService(StorageRepository saloonItem)
         {
             _saloonItem = saloonItem;
         }
-        public BaseDto AddItem(string name, double price, int quantityAvaliable, Guid itemId)
+
+
+
+        public BaseDto AddItem(string name, double price, int quantityAvaliable, Guid userId)
         {
-            var item = new SaloonItemEntity(name, price, quantityAvaliable);
-            _saloonItem.Add(item);
+            var item = new SaloonItemEntity(name, price, quantityAvaliable, userId);
+            _saloonItem.Create(item);
             return new BaseDto(200, "Item adicionado com sucesso");
         }
-        public BaseDto GetItems()
-        {
-            _saloonItem.GetAll();
-            return new BaseDto(200, "Exibindo todos os itens");
-        }
-        public BaseDto GetItemsId(Guid itemId)
-        {
-            _saloonItem.GetById(itemId);
-            return new BaseDto(200, "Item encontrado");
-        }
+
+
+
+        //public BaseDto GetItems()
+        //{
+        //    _saloonItem.GetAll();
+        //    return new BaseDto(200, "Exibindo todos os itens");
+        //}
+        //public BaseDto GetItemsId(Guid itemId)
+        //{
+        //    _saloonItem.GetById(itemId);
+        //    return new BaseDto(200, "Item encontrado");
+        //}
     }
 }
--- Hair.Domain/Entities/SaloonItemEntity.cs
+++ Hair.Domain/Entities/SaloonItemEntity.cs
@@ -4,12 +4,15 @@ namespace Hair.Domain.Entities
 {
     public class SaloonItemEntity : BaseEntity
     {
+        public Guid SaloonId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int QuantityAvaible { get; set; }
 
-        public SaloonItemEntity(string name, double price, int quantityAvaible)
+        public SaloonItemEntity(string name, double price, int quantityAvaible, Guid saloonId)
         {
+            SaloonId = saloonId;
+            Id = Guid.NewGuid();
             Name = name;
             Price = price;
             QuantityAvaible = quantityAvaible;
