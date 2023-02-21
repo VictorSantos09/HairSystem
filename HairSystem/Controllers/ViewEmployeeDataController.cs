6fbabe25ecedca7a71ea50d2632a2c4888f1a9db
Victor Santos <victorluizsantos49@gmail.com>
2023-02-20 22:57:16 -0300

    Ver Dados do Funcionario feito

---
+++ HairSystem/Controllers/ViewEmployeeDataController.cs
@@ -0,0 +1,33 @@
+﻿using Hair.Application.Common;
+using Hair.Application.Dto;
+using Hair.Application.Services;
+using Hair.Domain.Entities;
+using Hair.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSystem.Controllers
+{
+    [ApiController]
+    public class ViewEmployeeDataController : ControllerBase
+    {
+        private readonly VisualizeEmployeeDataService _getEmployeeData;
+        private readonly IBaseRepository<BarberEntity> _barberRepository;
+        private readonly IGetByEmail _userRepository;
+
+        public ViewEmployeeDataController(IBaseRepository<BarberEntity> barberRepository, IGetByEmail userRepository)
+        {
+            _barberRepository = barberRepository;
+            _userRepository = userRepository;
+            _getEmployeeData = new(_barberRepository, _userRepository);
+        }
+
+        [HttpPost]
+        [Route("VisualizeEmployeeData")]
+        public IActionResult Visualize([FromBody] VisualizeEmployeeDataDto dataDto)
+        {
+            var result = _getEmployeeData.GetEmployeeData(dataDto.Email, dataDto.Password);
+
+            return StatusCode(result._StatusCode, result._Data == null ? new MessageDto(result._Message): result._Data);
+        }
+    }
+}
