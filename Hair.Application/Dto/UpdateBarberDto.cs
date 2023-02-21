6beae3b320a3f416d68a749d34c409d1b50d6c4d
Victor Santos <victorluizsantos49@gmail.com>
2023-02-21 11:21:38 -0300

    gerenciamento do funcionário melhorado

---
+++ Hair.Application/Dto/UpdateBarberDto.cs
@@ -0,0 +1,33 @@
+﻿using Hair.Domain.Entities;
+
+namespace Hair.Application.Dto
+{
+    public class UpdateBarberDto
+    {
+        public Guid UserId { get; set; }
+        public string BarberName { get; set; }
+        public string? BarberEmail { get; set; }
+        public string? BarberPhoneNumber { get; set; }
+        public double BarberSalary { get; set; }
+        public AddressEntity NewAddress { get; set; }
+        public string? NewEmail { get; set; }
+        public string? NewPhoneNumber { get; set; }
+        public string NewName { get; set; }
+        public double NewSalary { get; set; }
+
+        public UpdateBarberDto(Guid userId, string barberName, string? barberEmail, string? barberPhoneNumber, double barberSalary, 
+            AddressEntity newAddress, string? newEmail, string? newPhoneNumber, string newName, double newSalary)
+        {
+            UserId = userId;
+            BarberName = barberName;
+            BarberEmail = barberEmail;
+            BarberPhoneNumber = barberPhoneNumber;
+            BarberSalary = barberSalary;
+            NewAddress = newAddress;
+            NewEmail = newEmail;
+            NewPhoneNumber = newPhoneNumber;
+            NewName = newName;
+            NewSalary = newSalary;
+        }
+    }
+}
---
+++ Hair.Application/Services/UpdateBarberService.cs
@@ -0,0 +1,59 @@
+﻿using Hair.Application.Common;
+using Hair.Application.Dto;
+using Hair.Application.Extensions;
+using Hair.Domain.Entities;
+using Hair.Repository.Interfaces;
+
+namespace Hair.Application.Services
+{
+    /// <summary>
+    /// Define os métodos para atualização de funcionário
+    /// </summary>
+    public class UpdateBarberService
+    {
+        private readonly IBaseRepository<UserEntity> _userRepository;
+        private readonly IBaseRepository<BarberEntity> _barberRepository;
+
+        public UpdateBarberService(IBaseRepository<UserEntity> userRepository, IBaseRepository<BarberEntity> barberRepository)
+        {
+            _userRepository = userRepository;
+            _barberRepository = barberRepository;
+        }
+
+        /// <summary>
+        /// 
+        /// Efetua a atualização de um novo barbeiro
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="dto">Dados necessários para atualizar</param>
+        /// 
+        /// <returns>Retorna <see cref="BaseDto"/> com mensagem e status code dependendo da condição encontrada</returns>
+        public BaseDto Update(UpdateBarberDto dto)
+        {
+            var user = _userRepository.GetById(dto.UserId);
+
+            if (user == null)
+                return BaseDtoExtension.NotFound();
+
+            var allBarbers = _barberRepository.GetAll().FindAll(x => x.SaloonId == user.Id);
+
+            if (allBarbers.Count == 0)
+                return BaseDtoExtension.Create(404, "Nenhum barbeiro foi encontrado");
+
+            var barberToUpdate = allBarbers.Find(x => x.Name == dto.BarberName || x.Email == dto.BarberEmail || x.PhoneNumber == dto.BarberPhoneNumber || x.Salary == dto.BarberSalary);
+
+            if (barberToUpdate == null)
+                return BaseDtoExtension.NotFound("Barbeiro para atualizar");
+
+            var barberUpdated = new BarberEntity(dto.NewName, dto.NewPhoneNumber, dto.NewEmail, dto.NewSalary, dto.NewAddress, true, user.Id, user.SaloonName);
+
+            barberToUpdate = barberUpdated;
+
+            _barberRepository.Update(barberToUpdate);
+
+            return BaseDtoExtension.Sucess($"Dados de {barberToUpdate.Name} atualizados");
+
+        }
+    }
+}
