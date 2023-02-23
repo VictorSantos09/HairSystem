a0b1ecf84bcabb50c33de1fc7feb94c708fc907c
Victor Santos <victorluizsantos49@gmail.com>
2023-02-22 21:30:06 -0300

    correção busca e envio do userEntity

--- Hair.Repository/Repositories/UserRepository.cs
+++ Hair.Repository/Repositories/UserRepository.cs
@@ -3,7 +3,11 @@ using Hair.Domain.Entities;
 using Hair.Repository.DataBase;
 using Hair.Repository.Interfaces;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
+using System.Reflection;
+using System.Security.Principal;
+using System.Xml.Linq;
 using static Dapper.SqlMapper;
 
 namespace Hair.Repository.Repositories
@@ -14,16 +18,19 @@ namespace Hair.Repository.Repositories
     public class UserRepository : BaseRepository<UserEntity>, IGetByEmail
     {
         private readonly static string TableName = "USERS";
+        private readonly IBaseRepository<HaircutEntity> _haircutRepository;
 
-        public UserRepository() : base(TableName)
+        public UserRepository(IBaseRepository<HaircutEntity> haircutRepository) : base(TableName)
         {
-
+            _haircutRepository = haircutRepository;
         }
+
         public void Create(UserEntity user)
         {
             using (var conn = new SqlConnection(DataAccess.DBConnection))
             {
-                var query = new SqlCommand($"INSERT INTO {TableName} VALUES (@ID, @SALOON_NAME, @OWNER_NAME, @PHONE_NUMBER, @EMAIL, @PASSWORD, @CNPJ, @HAIRCUT_TIME, @HAIRCUT_HAIR, @HAIRCUT_BEARD, @HAIRCUT_MUSTACHE)", conn);
+                var query = new SqlCommand($"INSERT INTO {TableName} VALUES (@ID, @SALOON_NAME, @OWNER_NAME, @PHONE_NUMBER, @EMAIL," +
+                    $" @PASSWORD, @CNPJ, @HAIRCUT_HAIR, @HAIRCUT_BEARD, @HAIRCUT_MUSTACHE)", conn);
 
                 conn.Open();
 
@@ -34,10 +41,30 @@ namespace Hair.Repository.Repositories
                 query.Parameters.AddWithValue("@EMAIL", user.Email);
                 query.Parameters.AddWithValue("@PASSWORD", user.Password);
                 query.Parameters.AddWithValue("@CNPJ", user.CNPJ);
-                query.Parameters.AddWithValue("@HAIRCUT_TIME", "02/04/2004"); // remover e fazer salvar o haircute time na tabela de haircute e sempre popular a lista do usuario
                 query.Parameters.AddWithValue("@HAIRCUT_HAIR", user.Prices.Hair);
                 query.Parameters.AddWithValue("@HAIRCUT_BEARD", user.Prices.Beard);
                 query.Parameters.AddWithValue("@HAIRCUT_MUSTACHE", user.Prices.Mustache);
+
+                query.ExecuteNonQuery();
+            }
+        }
+        public void Update(UserEntity user)
+        {
+            using (IDbConnection conn = new SqlConnection(DataAccess.DBConnection))
+            {
+                var query = new SqlCommand($"UPDATE {TableName} SET SALOON_NAME = @SaloonName, OWNER_NAME = @OwnerName, PHONE_NUMBER = @PhoneNumber, EMAIL = @Email, " +
+                    $"PASSWORD = @Password, CNPJ = @CNPJ, HAIRCUT_PRICE = @Prices WHERE @ID = Id)");
+
+                conn.Open();
+
+                query.Parameters.AddWithValue("@SALOON_NAME", user.SaloonName);
+                query.Parameters.AddWithValue("@OWNER_NAME", user.OwnerName);
+                query.Parameters.AddWithValue("@PHONE_NUMBER", user.PhoneNumber);
+                query.Parameters.AddWithValue("@EMAIL", user.Email);
+                query.Parameters.AddWithValue("@PASSWORD", user.Password);
+                query.Parameters.AddWithValue("@CNPJ", user.CNPJ);
+                query.Parameters.AddWithValue("@HAIRCUT_PRICE", user.Prices.Hair);
+                query.Parameters.AddWithValue("@ID", user.Id);
 
                 query.ExecuteNonQuery();
             }
@@ -47,34 +74,102 @@ namespace Hair.Repository.Repositories
         {
             using (var conn = new SqlConnection(DataAccess.DBConnection))
             {
-                var output = conn.Query<UserEntity>($" SELECT * FROM {TableName} WHERE EMAIL = '{email.ToUpper()}' AND PASSWORD = '{password}'").ToList();
+                var query = $" SELECT * FROM {TableName} WHERE EMAIL= @EMAIL AND PASSWORD= @PASSWORD";
 
+                SqlCommand cmd = new SqlCommand(query, conn);
 
-                return output.Count == 0 ? null : output.FirstOrDefault();
-            }
-        }
-
-        public void Update(UserEntity user)
-        {
-            using (IDbConnection conn = new SqlConnection(DataAccess.DBConnection))
-            {
-                var query = new SqlCommand($"UPDATE {TableName} SET SALOON_NAME = @SaloonName, OWNER_NAME = @OwnerName, PHONE_NUMBER = @PhoneNumber, EMAIL = @Email, " +
-                    $"PASSWORD = @Password, CNPJ = @CNPJ, HAIRCUT_TIME = @HaircuteTime, HAIRCUT_PRICE = @Prices WHERE @ID = Id)");
+                cmd.Parameters.AddWithValue("@EMAIL", email.ToUpper());
+                cmd.Parameters.AddWithValue("@PASSWORD", password);
 
                 conn.Open();
 
-                query.Parameters.AddWithValue("@SALOON_NAME", user.SaloonName);
-                query.Parameters.AddWithValue("@OWNER_NAME", user.OwnerName);
-                query.Parameters.AddWithValue("@PHONE_NUMBER", user.PhoneNumber);
-                query.Parameters.AddWithValue("@EMAIL", user.Email);
-                query.Parameters.AddWithValue("@PASSWORD", user.Password);
-                query.Parameters.AddWithValue("@CNPJ", user.CNPJ);
-                query.Parameters.AddWithValue("@HAIRCUT_TIME", "02/04/2004");
-                query.Parameters.AddWithValue("@HAIRCUT_PRICE", user.Prices.Hair);
-                query.Parameters.AddWithValue("@ID", user.Id);
-
-                query.ExecuteNonQueryAsync();
+                return BuildEntity(cmd);
             }
+        }
+
+        //public UserEntity? GetById(Guid id)
+        //{
+        //    using (var conn = new SqlConnection(DataAccess.DBConnection))
+        //    {
+        //        var query = $"SELECT * FROM {TableName} WHERE Id= @Id";
+
+        //        SqlCommand cmd = new SqlCommand(query, conn);
+
+        //        cmd.Parameters.AddWithValue("@Id", id);
+
+        //        conn.Open();
+
+        //        return BuildEntity(cmd);
+        //    }
+        //}
+
+        //public List<UserEntity> GetAll()
+        //{
+        //    using (var conn = new SqlConnection(DataAccess.DBConnection))
+        //    {
+        //        var query = $"SELECT * FROM {TableName}";
+        //        SqlCommand cmd = new SqlCommand(query, conn);
+
+        //        conn.Open();
+
+        //        var users = new List<UserEntity>();
+
+        //        using (SqlDataReader reader = cmd.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                var user = new UserEntity();
+
+        //                user.Id = reader.GetGuid("ID");
+        //                user.Password = reader.GetString("PASSWORD");
+        //                user.CNPJ = reader.GetString("CNPJ");
+        //                user.Email = reader.GetString("EMAIL");
+        //                user.OwnerName = reader.GetString("OWNER_NAME");
+        //                user.PhoneNumber = reader.GetString("PHONE_NUMBER");
+        //                user.SaloonName = reader.GetString("SALOON_NAME");
+        //                user.Prices.Hair = reader.GetDouble("HAIRCUT_HAIR");
+        //                user.Prices.Mustache = reader.GetDouble("HAIRCUT_MUSTACHE");
+        //                user.Prices.Beard = reader.GetDouble("HAIRCUT_BEARD");
+
+        //                PopulateHaircut(user);
+
+        //                users.Add(user);
+        //            }
+        //        }
+        //        return users;
+        //    }
+        //}
+
+        private UserEntity? BuildEntity(SqlCommand cmd)
+        {
+            var user = new UserEntity();
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    user.Id = reader.GetGuid("ID");
+                    user.Password = reader.GetString("PASSWORD");
+                    user.CNPJ = reader.GetString("CNPJ");
+                    user.Email = reader.GetString("EMAIL");
+                    user.OwnerName = reader.GetString("OWNER_NAME");
+                    user.PhoneNumber = reader.GetString("PHONE_NUMBER");
+                    user.SaloonName = reader.GetString("SALOON_NAME");
+                    user.Prices.Hair = reader.GetDouble("HAIRCUT_HAIR");
+                    user.Prices.Mustache = reader.GetDouble("HAIRCUT_MUSTACHE");
+                    user.Prices.Beard = reader.GetDouble("HAIRCUT_BEARD");
+
+                }
+            }
+
+            PopulateHaircut(user);
+            return user;
+        }
+        private void PopulateHaircut(UserEntity user)
+        {
+            var haircuts = _haircutRepository.GetAll().FindAll(x => x.SaloonId == user.Id);
+
+            user.Haircuts.AddRange(haircuts);
         }
     }
 }
