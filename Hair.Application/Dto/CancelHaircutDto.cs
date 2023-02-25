fa1357860f298d16e783d4fb61a29fa2798eff98
Victor Santos <victorluizsantos49@gmail.com>
2023-02-24 22:29:31 -0300

    cancelar corte feito

---
+++ Hair.Application/Dto/CancelHaircutDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Hair.Application.Dto
+{
+    public class CancelHaircutDto
+    {
+        public Guid UserID { get; set; }
+        public bool Confirmed { get; set; }
+        public string ClientName { get; set; }
+        public string? ClientEmail { get; set; }
+        public string ClientPhoneNumber { get; set; }
+        public DateTime HaircutTime { get; set; }
+
+        public CancelHaircutDto(Guid userID, bool confirmed, string clientName, string? clientEmail, string clientPhoneNumber, DateTime haircutTime)
+        {
+            UserID = userID;
+            Confirmed = confirmed;
+            ClientName = clientName;
+            ClientEmail = clientEmail;
+            ClientPhoneNumber = clientPhoneNumber;
+            HaircutTime = haircutTime;
+        }
+    }
+}
