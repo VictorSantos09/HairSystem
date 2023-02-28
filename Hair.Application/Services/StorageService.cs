897a5e0a0b16d2b6f7859a3591ea3b08ad86dada
DESKTOP-VRG87DV\Matheus <avelinosantos09@gmail.com>
2023-02-27 21:25:44 -0300

    Implementação do método de remoção de item - Adicionado verificações para nome, Id e quantidade de itens
    - *Próximos passos*:
    - Implementar Controller para testar métodos;
    - Implementar testes

--- Hair.Application/Services/StorageService.cs
+++ Hair.Application/Services/StorageService.cs
@@ -6,6 +6,7 @@ using Hair.Repository.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,32 +28,44 @@ namespace Hair.Application.Services
         public BaseDto AddItem(string name, double price, int quantityAvaliable, Guid userId)
         {
             if (quantityAvaliable <= 0)
-            return BaseDtoExtension.Invalid("Quantidade deve ser maior que 0 (zero)");
+                return BaseDtoExtension.Invalid("Quantidade deve ser maior que 0 (zero)");
 
             if (name == null)
-            return BaseDtoExtension.NotNull("Nome do produto");
+                return BaseDtoExtension.NotNull("Nome do produto");
 
             if (price <= 0)
                 return BaseDtoExtension.Invalid("O valor do Produto deve ser superior a R$0,00");
 
             var item = new SaloonItemEntity(name, price, quantityAvaliable, userId);
-            _saloonItem.Create(item);
             var user = _userRepository.GetById(userId);
             if (user != null)
                 return BaseDtoExtension.NotFound();
 
-            //user.Itens.Add(item);             { Métodos descartados pelas mudanças na StorageRepository
-            //_userRepository.Update(user);     }   
+            _saloonItem.Create(item);
             return BaseDtoExtension.Sucess();
         }
         public BaseDto GetItems(Guid userId)
         {
-            // A linha 51 substitui a instância da List<SaloonItemEntity>
-            var itemList = _saloonItem.GetAll().FindAll(x => x.Id == userId); // Pega todos os itens do DB e filtra pelo ID do usuário
+            var user = _userRepository.GetById(userId);
+            if (user != null)
+                return BaseDtoExtension.NotFound();
+            var itemList = _saloonItem.GetAll().FindAll(x => x.Id == userId);
             if (itemList.Count == 0)
                 return BaseDtoExtension.Sucess("Nenhum item foi encontrado");
-           
             return BaseDtoExtension.Create(200, "Exibindo todos os itens", itemList);
+        }
+        public BaseDto RemoveItems(Guid userId, string itemName, int itemAmount, Guid itemId)
+        {
+            var user = _userRepository.GetById(userId);
+            if (user != null)
+                return BaseDtoExtension.NotFound();
+            var item = user.Itens.Find(x => x.Id == itemId && x.Name == itemName && x.QuantityAvaible == itemAmount);
+
+            if (item == null)
+                return BaseDtoExtension.NotFound("Item");
+            _saloonItem.Remove(item.Id);
+
+            return BaseDtoExtension.Sucess("Item removido com sucesso");
         }
     }
 }
